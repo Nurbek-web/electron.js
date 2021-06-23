a4c3be6b28a6e2c2b2efa51c30c58d4a34ccd2b9
Nurbek-web <taizhanovntm@gmail.com>
2021-06-23 23:04:46 +0600

    sadas

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const {Notification} = require("electron");
+const { app, BrowserWindow, Menu, Tray } = require("electron");
+const path = require("path");
+const { Notification } = require("electron");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
   app.quit();
 }
 
 app.setUserTasks([
   {
     program: process.execPath,
-    arguments: '--new-window',
+    arguments: "--new-window",
     iconPath: process.execPath,
     iconIndex: 0,
-    title: 'New Window',
-    description: 'Create a new window'
-  }
-])
+    title: "New Window",
+    description: "Create a new window",
+  },
+]);
 
 const createWindow = () => {
   // Create the browser window.
@@ -26,39 +27,57 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
-  mainWindow.setOverlayIcon(path.join(__dirname, "image.png"), 'Description for overlay')
-  console.log(path.join(__dirname, "image.png"))
+  mainWindow.setOverlayIcon(
+    path.join(__dirname, "image.png"),
+    "Description for overlay"
+  );
+  console.log(path.join(__dirname, "image.png"));
 };
-
-
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-const NOTIFICATION_TITLE = 'Basic Notification'
-const NOTIFICATION_BODY = 'Notification from the Main process'
+const NOTIFICATION_TITLE = "Basic Notification";
+const NOTIFICATION_BODY = "Notification from the Main process";
 
-function showNotification () {
-  new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
+function showNotification() {
+  new Notification({
+    title: NOTIFICATION_TITLE,
+    body: NOTIFICATION_BODY,
+  }).show();
 }
 
-app.whenReady().then(createWindow).then(showNotification)
+app
+  .whenReady()
+  .then(createWindow)
+  .then(showNotification)
+  .then(() => {
+    tray = new Tray(path.join(__dirname, "image.png"));
+    const contextMenu = Menu.buildFromTemplate([
+      { label: "Item1", type: "radio" },
+      { label: "Item2", type: "radio" },
+      { label: "Item3", type: "radio", checked: true },
+      { label: "Item4", type: "radio" },
+    ]);
+    tray.setToolTip("This is my application.");
+    tray.setContextMenu(contextMenu);
+  });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
