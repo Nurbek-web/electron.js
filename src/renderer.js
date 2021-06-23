b915f3ce4460cff36596a0295f28e11d30715c28
Nurbek-web <taizhanovntm@gmail.com>
2021-06-23 17:09:03 +0600

    first commit

---
+++ src/renderer.js
@@ -0,0 +1,8 @@
+// Notification system
+
+// const NOTIFICATION_TITLE = 'Title'CLICK_MESSAGE
+// const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.'
+// const  = 'Notification clicked'
+
+// new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
+//   .onclick = () => console.log(CLICK_MESSAGE)
